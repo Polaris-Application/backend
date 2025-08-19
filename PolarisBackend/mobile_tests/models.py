aa32d0c06fcef4a027670c9c3b9a2ecf65f3148a
zahra121281 <zahra121281alizadeh@gmail.com>
2025-08-20 01:17:43 +0330

    hotfix: correct name choices in UserTest model

--- PolarisBackend/mobile_tests/models.py
+++ PolarisBackend/mobile_tests/models.py
@@ -6,19 +6,20 @@ class UserMobileTests(models.Model):
     DNS = 'dns'
     SMS = 'sms'
     UP = 'up'
-    DOWN = 'down'
+    DOWN = 'download'
     WEB = 'web'
     test_choice= (
         (PING,'ping'),
         (SMS, 'sms'),
-        (DOWN , 'down'),
+        (DOWN , 'download'),
         (WEB, 'web'),
         (UP, 'up'),
         (DNS, 'dns')
     )
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="mobile_tests")
-    name = models.CharField(blank=False, null=False,max_length=6, choices=test_choice)
+    name = models.CharField(blank=False, null=False,max_length=10, choices=test_choice)
     timestamp = models.DateTimeField()
     test_domain = models.CharField(blank=True, null=True)
     result = models.FloatField()
 
+{'name': 'download', 'result': -1.0, 'timestamp': '2025-08-20T01:08:59'}
